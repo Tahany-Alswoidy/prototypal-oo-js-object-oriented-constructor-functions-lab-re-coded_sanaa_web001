f29b317578ad44661abcdf4fc73f1001ce33fadf
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-17 08:53:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function Scooter (year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+function Scooter (year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+function Driver  (year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
